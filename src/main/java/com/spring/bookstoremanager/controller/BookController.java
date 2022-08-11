15eb8907fdef5285d3abfc07213a6c40cfba6385
jucatupinamba <juniorbmx13@gmail.com>
2022-08-11 19:20:50 -0300

    feat: add classe Service, refactoramento de métodos para a camada service

--- src/main/java/com/spring/bookstoremanager/controller/BookController.java
+++ src/main/java/com/spring/bookstoremanager/controller/BookController.java
@@ -3,6 +3,7 @@ package com.spring.bookstoremanager.controller;
 import com.spring.bookstoremanager.dto.MessageResponseDTO;
 import com.spring.bookstoremanager.entity.Book;
 import com.spring.bookstoremanager.repository.BookRepository;
+import com.spring.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,18 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/books")
 public class BookController {
 
-    private BookRepository bookRepository;
+    private BookService bookService;
     @Autowired
-    public BookController(BookRepository bookRepository){
-        this.bookRepository = bookRepository;
+    public BookController(final BookService bookService) {
+        this.bookService = bookService;
     }
-    @PostMapping
-    public MessageResponseDTO create(@RequestBody Book book){
-        Book savedBook = bookRepository.save(book);
-        return MessageResponseDTO.builder()
-                .message("Book created with ID " + savedBook.getId())
-                .build();
 
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody Book book) {
+        return bookService.create(book);
     }
 
 }
---
+++ src/main/java/com/spring/bookstoremanager/service/BookService.java
@@ -0,0 +1,25 @@
+package com.spring.bookstoremanager.service;
+
+import com.spring.bookstoremanager.dto.MessageResponseDTO;
+import com.spring.bookstoremanager.entity.Book;
+import com.spring.bookstoremanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Service
+public class BookService {
+
+    private BookRepository bookRepository;
+    @Autowired
+    public BookService(final BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public MessageResponseDTO create(Book book){
+        Book savedBook = bookRepository.save(book);
+        return MessageResponseDTO.builder()
+                .message("Book created with ID " + savedBook.getId())
+                .build();
+    }
+}
